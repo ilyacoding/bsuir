d7faa316875ff78ecc680dbce97d466ef9a52f38
IlyaCoding <ilyacoding@yandex.com>
2017-02-17 02:15:36 +0300

    Initialized KSiS Lab_1

---
+++ KSiS/Lab_1/Client/Client/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+
+    public partial class Form1 : Form
+    {
+        Socket sock;
+        public Form1()
+        {
+            InitializeComponent();
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint localIP = new IPEndPoint(IPAddress.Parse(GetLocalIPAddress()), 7777);
+            sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1);
+            sock.Bind(localIP);
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] data = new byte[256];
+            string str;
+
+            sock.Receive(data);
+            str = Encoding.ASCII.GetString(data);
+
+            if (str.Length > 0)
+            {
+                label1.Text = str;
+                timer1.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            button1_Click(sender, e);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ KSiS/Lab_1/Server/Server/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        Socket sock;
+        public Form1()
+        {
+            InitializeComponent();
+
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+        }
+       
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString();
+            byte[] data = Encoding.ASCII.GetBytes(label1.Text);
+            IPEndPoint toIP = new IPEndPoint(IPAddress.Broadcast, 7777);
+            //string IPBroad = "25.87.255.255";
+            //IPEndPoint toIP = new IPEndPoint(IPAddress.Parse(IPBroad), 7777);
+            label2.Text = IPAddress.Broadcast.ToString();
+            sock.SendTo(data, toIP);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+    }
+}
